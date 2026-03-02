e3deccc184dd8059e69c765e86f41a8e7b96bca3
Nicolly <nicolly.l@edu.senai.br>
2026-03-02 17:15:17 -0300

    Add: criou jogo mad libs

---
+++ 01_mad_libs/JogoTeca.py
@@ -0,0 +1,28 @@
+print("""
+@@@@@@      @@@  @@@  @@@   @@@@@@   @@@@@@@  @@@@@@@   @@@   @@@@@@      @@@@@@@@@@    @@@@@@   @@@       @@@  @@@   @@@@@@@   @@@@@@   
+@@@@@@@@     @@@  @@@  @@@  @@@@@@@   @@@@@@@  @@@@@@@@  @@@  @@@@@@@@     @@@@@@@@@@@  @@@@@@@@  @@@       @@@  @@@  @@@@@@@@  @@@@@@@@  
+@@!  @@@     @@!  @@@  @@!  !@@         @@!    @@!  @@@  @@!  @@!  @@@     @@! @@! @@!  @@!  @@@  @@!       @@!  @@@  !@@       @@!  @@@  
+!@!  @!@     !@!  @!@  !@!  !@!         !@!    !@!  @!@  !@!  !@!  @!@     !@! !@! !@!  !@!  @!@  !@!       !@!  @!@  !@!       !@!  @!@  
+@!@!@!@!     @!@!@!@!  !!@  !!@@!!      @!!    @!@!!@!   !!@  @!@!@!@!     @!! !!@ @!@  @!@!@!@!  @!!       @!@  !@!  !@!       @!@!@!@!  
+!!!@!!!!     !!!@!!!!  !!!   !!@!!!     !!!    !!@!@!    !!!  !!!@!!!!     !@!   ! !@!  !!!@!!!!  !!!       !@!  !!!  !!!       !!!@!!!!  
+!!:  !!!     !!:  !!!  !!:       !:!    !!:    !!: :!!   !!:  !!:  !!!     !!:     !!:  !!:  !!!  !!:       !!:  !!!  :!!       !!:  !!!  
+:!:  !:!     :!:  !:!  :!:      !:!     :!:    :!:  !:!  :!:  :!:  !:!     :!:     :!:  :!:  !:!   :!:      :!:  !:!  :!:       :!:  !:!  
+::   :::     ::   :::   ::  :::: ::      ::    ::   :::   ::  ::   :::     :::     ::   ::   :::   :: ::::  ::::: ::   ::: :::  ::   :::  
+ :   : :      :   : :  :    :: : :       :      :   : :  :     :   : :      :      :     :   : :  : :: : :   : :  :    :: :: :   :   : :  
+""")
+
+
+
+verbo= input("digite um verbo: ")
+animal=input("digite um animal: ")
+adjetivo=input("digite um adjetivo: ")
+objeto=input("digite um objeto: ")
+lugar=input("digite um lugar: ")
+emoção=input("digite uma emoção: ")
+
+#imprimindo a históia maluca para o usuário
+print(f"""Era uma vez um {animal}que adorava brincar, em um belo dia ele encontrou algo muito estranho e quando ele olhou muito bem para o {lugar}, ele achou muito estranho e quando
+se aproximou ele viu um(a) {objeto} e quando ele tocou nesse na {objeto} ele se teletransportou para outro mundo, que tinha uma floresta muito bonita porém não havia ninguém ali e o {animal}
+ficou{emoção}. Depois desse dia o {animal} não via nada além  daquela grande floresta, mas o {animal} era extremamente {adjetivo} e não desistiu e em uma noite ele saiu{verbo}pela floresta
+e caiu em um burraco  e voltou para o seu mundo ele ficou muito {emoção}.
+""")
